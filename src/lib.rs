15e70dbf62d1025d4c5065bea2cf0ef3c536c8b5
Ulf Lilleengen <lulf@redhat.com>
2022-06-08 12:08:24 +0200

    Make types available on stable rust

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,34 @@
 //! Types for updating firmware of embedded devices from a remote server. The protocol is not
 //! tied to any specific platform.
 #![no_std]
-#![feature(generic_associated_types)]
-#![feature(type_alias_impl_trait)]
+#![cfg_attr(feature = "full", feature(generic_associated_types))]
+#![cfg_attr(feature = "full", feature(type_alias_impl_trait))]
 
 mod fmt;
 
 mod protocol;
 pub use protocol::*;
+
+#[cfg(feature = "full")]
 mod device;
+
+#[cfg(feature = "full")]
 pub use device::*;
+
+#[cfg(feature = "full")]
 mod service;
+
+#[cfg(feature = "full")]
 pub use service::*;
+
+#[cfg(feature = "full")]
 mod traits;
+
+#[cfg(feature = "full")]
 pub use traits::*;
+
+#[cfg(feature = "full")]
 mod updater;
+
+#[cfg(feature = "full")]
 pub use updater::*;
