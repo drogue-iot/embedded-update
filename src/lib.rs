768a96b59b9a380ead7ef6b4fd837ae0fa926a5c
Ulf Lilleengen <lulf@redhat.com>
2022-06-14 09:42:49 +0200

    Rename feature to nightly

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,34 @@
 //! Types for updating firmware of embedded devices from a remote server. The protocol is not
 //! tied to any specific platform.
 #![no_std]
-#![cfg_attr(feature = "full", feature(generic_associated_types))]
-#![cfg_attr(feature = "full", feature(type_alias_impl_trait))]
+#![cfg_attr(feature = "nightly", feature(generic_associated_types))]
+#![cfg_attr(feature = "nightly", feature(type_alias_impl_trait))]
 
 mod fmt;
 
 mod protocol;
 pub use protocol::*;
 
-#[cfg(feature = "full")]
+#[cfg(feature = "nightly")]
 mod device;
 
-#[cfg(feature = "full")]
+#[cfg(feature = "nightly")]
 pub use device::*;
 
-#[cfg(feature = "full")]
+#[cfg(feature = "nightly")]
 mod service;
 
-#[cfg(feature = "full")]
+#[cfg(feature = "nightly")]
 pub use service::*;
 
-#[cfg(feature = "full")]
+#[cfg(feature = "nightly")]
 mod traits;
 
-#[cfg(feature = "full")]
+#[cfg(feature = "nightly")]
 pub use traits::*;
 
-#[cfg(feature = "full")]
+#[cfg(feature = "nightly")]
 mod updater;
 
-#[cfg(feature = "full")]
+#[cfg(feature = "nightly")]
 pub use updater::*;
