cd7ef2912a074a95421d9856e46f29599a0ffacd
Ulf Lilleengen <lulf@redhat.com>
2022-06-29 10:16:44 +0200

    poc hawkbit update service

--- src/service/mod.rs
+++ src/service/mod.rs
@@ -1,10 +1,14 @@
 //! Implementations of the `UpdateService` trait.
 #[cfg(feature = "drogue")]
 mod drogue;
+#[cfg(feature = "hawkbit")]
+mod hawkbit;
 mod memory;
 mod serial;
 
 #[cfg(feature = "drogue")]
 pub use drogue::*;
+#[cfg(feature = "hawkbit")]
+pub use hawkbit::*;
 pub use memory::*;
 pub use serial::*;
